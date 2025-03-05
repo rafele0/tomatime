4148e3640d03e3f279341454e9fda630c5062198
raff0909 <raffaele.severino9@gmail.com>
2025-03-05 23:02:46 +0100

    proseguio fe

--- react/vite-project/src/components/WorkingSection.jsx
+++ react/vite-project/src/components/WorkingSection.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TaskItem from './TaskItem';
+import working from '../assets/titleWorkingAt.svg';
+import arrowLeft from '../assets/arrowLeftWorking.svg';
+import arrowRight from '../assets/arrowRightWorking.svg';
 
 function WorkingSection({ tasks, onCompleteTask }) {
   return (
+  <div className="intWorking">
     <div className="working-section">
 
       <span className="section-titleWorkingAt">TIME TO FOCUS</span>
@@ -17,17 +21,20 @@ function WorkingSection({ tasks, onCompleteTask }) {
           />
         ))}
       </ul>
-      <div className='working-section__footer'>
-          <span>WORKING AT</span>
+      
 
-          <div className='section-for-task'>
-              
-             
-          </div>
-        <button className='TodoRetrunBnt'>todo</button>
-        <button className='DoneNextBnt'>done</button>
       </div>
-    </div>
+      <img src={working} className='workingAt'/>
+      <div className='section-for-task'></div>
+
+      <button className='btnToDo'> <img src={arrowLeft} style={{"width":"20%", "height":"70%", "position":"absolute","justifyContent":"center", "top":"5px", "marginInline":"-40px"}}/> To Do </button>
+      <button className='btnDone'>Done <img src={arrowRight} style={{"width":"20%", "height":"70%", "position":"absolute","justifyContent":"center", "top":"5px", "marginInline":"20px"}}/></button>
+
+
+      
+
+</div>
+    
   );
 }
 
